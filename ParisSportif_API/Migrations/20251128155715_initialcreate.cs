683c4d78b65de479e1be74bdc96e096c31e605d4
ZzVf <goldtime79.1979@gmail.com>
2025-11-28 17:19:06 +0100

    V0.3 - InitialCreate
    -Première migrations

---
+++ ParisSportif_API/Migrations/20251128155715_initialcreate.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ParisSportif_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Points = table.Column<int>(type: "integer", nullable: false),
+                    isBlocked = table.Column<bool>(type: "boolean", nullable: false),
+                    isAdmin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ligues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Logo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ligues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientLigue",
+                columns: table => new
+                {
+                    FavoriteLiguesId = table.Column<int>(type: "integer", nullable: false),
+                    FavoritesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientLigue", x => new { x.FavoriteLiguesId, x.FavoritesId });
+                    table.ForeignKey(
+                        name: "FK_ClientLigue_Clients_FavoritesId",
+                        column: x => x.FavoritesId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClientLigue_Ligues_FavoriteLiguesId",
+                        column: x => x.FavoriteLiguesId,
+                        principalTable: "Ligues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Logo = table.Column<string>(type: "text", nullable: false),
+                    Ranking = table.Column<int>(type: "integer", nullable: false),
+                    LigueId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clubs_Ligues_LigueId",
+                        column: x => x.LigueId,
+                        principalTable: "Ligues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientClub",
+                columns: table => new
+                {
+                    FavoriteClubsId = table.Column<int>(type: "integer", nullable: false),
+                    FavoritesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientClub", x => new { x.FavoriteClubsId, x.FavoritesId });
+                    table.ForeignKey(
+                        name: "FK_ClientClub_Clients_FavoritesId",
+                        column: x => x.FavoritesId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClientClub_Clubs_FavoriteClubsId",
+                        column: x => x.FavoriteClubsId,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Score1 = table.Column<int>(type: "integer", nullable: false),
+                    Score2 = table.Column<int>(type: "integer", nullable: false),
+                    MatchDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    isTopMatch = table.Column<bool>(type: "boolean", nullable: false),
+                    isCanceled = table.Column<bool>(type: "boolean", nullable: false),
+                    ClubId1 = table.Column<int>(type: "integer", nullable: false),
+                    ClubId2 = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Clubs_ClubId1",
+                        column: x => x.ClubId1,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Clubs_ClubId2",
+                        column: x => x.ClubId2,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Point = table.Column<int>(type: "integer", nullable: false),
+                    BetTiming = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    isVictory = table.Column<bool>(type: "boolean", nullable: false),
+                    ClientId = table.Column<int>(type: "integer", nullable: false),
+                    MatchId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bets_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bets_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_ClientId",
+                table: "Bets",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_MatchId",
+                table: "Bets",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientClub_FavoritesId",
+                table: "ClientClub",
+                column: "FavoritesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientLigue_FavoritesId",
+                table: "ClientLigue",
+                column: "FavoritesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clubs_LigueId",
+                table: "Clubs",
+                column: "LigueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clubs_Ranking_LigueId",
+                table: "Clubs",
+                columns: new[] { "Ranking", "LigueId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_ClubId1",
+                table: "Matches",
+                column: "ClubId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_ClubId2",
+                table: "Matches",
+                column: "ClubId2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bets");
+
+            migrationBuilder.DropTable(
+                name: "ClientClub");
+
+            migrationBuilder.DropTable(
+                name: "ClientLigue");
+
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Clubs");
+
+            migrationBuilder.DropTable(
+                name: "Ligues");
+        }
+    }
+}
